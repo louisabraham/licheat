ff13f5b6649ffe4044a951bb095151899757dc96
louisabraham <louis.abraham@yahoo.fr>
2024-11-07 16:41:15 +0100

    ud

--- licheat.py
+++ licheat.py
@@ -3,7 +3,7 @@ from mitmproxy import http
 
 def response(flow: http.HTTPFlow) -> None:
     # Check if the request URL matches the desired pattern
-    if flow.request.url.startswith("https://lichess1.org/assets/compiled/site"):
+    if flow.request.url.startswith("https://lichess1.org/assets/compiled"):
         # Modify the JavaScript content
         modified_js_content = flow.response.content.replace(
             b"trustAllEvents:!1", b"trustAllEvents:!0"
